d8f853f3a6a99e163ab5f76a0115b307ceee6aa0
Dimitri <dimitri@formitize.com>
2021-11-12 08:48:17 +1100

    work from last night

--- Assets/Scripts/WaterLog.cs
+++ Assets/Scripts/WaterLog.cs
@@ -4,10 +4,30 @@ using UnityEngine;
 
 public class WaterLog : MonoBehaviour
 {
+    [SerializeField] List<GameObject> LogPrefabs;
     // Start is called before the first frame update
+    List<GameObject> spawnedLogs;
+
+    
     void Start()
     {
-        
+        //this.transform.localScale.x;
+
+        spawnedLogs = new List<GameObject>();
+
+        foreach(var item in LogPrefabs) {
+            var log = GameObject.Instantiate(
+                item,
+                Vector3.zero,
+                Quaternion.identity,
+                this.transform
+            );
+
+            log.SetActive(false);
+            
+            spawnedLogs.Add(log);
+        }
+
     }
 
     // Update is called once per frame
